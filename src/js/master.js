2f149f1c12b1ff4c0fa3ae18f2674aea5af91fdb
mariolo1985 <loserpunkx182@gmail.com>
2018-10-06 23:20:52 -0600

    homepage service animation

--- src/js/master.js
+++ src/js/master.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { callbackAtElementSurface } from 'scroll-callback';
 
 import { Menu, Footer } from '../components';
 
+function bumpHomepageClient(el) {
+    el.classList.add('bump');
+}
+
+function addCarouselEvent() {
+    const homepageClient = document.getElementById('homepage-client-carousel');
+    if (homepageClient !== null) {
+        callbackAtElementSurface(bumpHomepageClient, 500, 'homepage-client-carousel');
+    }
+    window.removeEventListener('scroll', addCarouselEvent);
+}
+
+function showHomepageService(el) {
+    el.classList.add('open');
+}
 
 (() => {
     const headerContainer = document.getElementById('header-container');
@@ -19,5 +35,10 @@ import { Menu, Footer } from '../components';
             <Footer />,
             footerContainer
         );
+    }
+
+    const homepageService = document.getElementById('homepage-services');
+    if (homepageService !== null) {
+        callbackAtElementSurface(showHomepageService, 500, 'homepage-services');
     }
 })();
