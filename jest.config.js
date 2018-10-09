a735eafd9eb121f17acb5af5009d63719f169f77
Mario Lo <kinfonglo@overstock.com>
2018-10-09 16:12:11 -0600

    added enzyme for component testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 // jest.config.js
 module.exports = {
     roots: ['<rootDir>/src'],
+    setupTestFrameworkScriptFile: '<rootDir>/enzyme.config.js',
     transform: {
         '^.+\\.js$': 'babel-jest'
     },
