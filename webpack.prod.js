07f1d12c4e49e28daee9199173bbbe2bbaf0c3b4
mariolo1985 <loserpunkx182@gmail.com>
2018-12-15 19:26:14 -0700

    migrating to webpack

---
+++ webpack.prod.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const entries = [
+    path.join(__dirname, 'src/js/master.js'),
+    path.join(__dirname, 'src/less/master.less')
+];
+
+module.exports = {
+    name: 'Bundling prod',
+    mode: 'production',
+    entry: entries,
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'js/master.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/master.min.css'
+        }),
+        new CopyWebpackPlugin([
+            { from: 'src/*.html', to: path.join(__dirname, 'dist'), flatten: true },
+            { from: 'src/data', to: path.join(__dirname, 'dist/data'), flatten: true },
+            { from: 'src/images', to: path.join(__dirname, 'dist/images'), flatten: true }
+        ])
+    ]
+};
